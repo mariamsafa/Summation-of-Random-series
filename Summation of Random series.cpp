1c3ca165268760f33be450fa772b9f69c344c950
Mariam Safa <88893709+mariamsafa@users.noreply.github.com>
2021-11-30 23:30:29 +0600

    summation

--- Summation of Random series.cpp
+++ Summation of Random series.cpp
@@ -1,3 +1,4 @@
+/*
 //Summation of 1/1^2 + 1/2^2 + 1/3^2 +....+1/N^2 series
 #include<stdio.h>
 #include<math.h>
@@ -31,6 +32,44 @@ int main()
 		sum = sum + 1 / pow(i, i);
 	}
 	printf("\nThe summation of this series is %lf \n", sum);
+
+
+	return 0;
+}
+
+//summation of 10^2 + 20^2 + 30^2 +....+N^2 series
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int i, n;
+	double sum = 0;
+	printf("Enter the number : ");
+	scanf_s("%d", &n);
+	for (i = 10; i <= n; i+=10)
+	{
+		sum = sum + pow(i, 2);
+	}
+	printf("\nThe summation of this series is %.2lf \n", sum);
+
+
+	return 0;
+}
+*/
+//summation of 100^2+95^2+90^2+.....+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int i, n;
+	double sum = 0;
+	printf("Enter the number : ");
+	scanf_s("%d", &n);
+	for (i = 10; i <= n; i += 10)
+	{
+		sum = sum + pow(i, 2);
+	}
+	printf("\nThe summation of this series is %.2lf \n", sum);
 
 
 	return 0;
