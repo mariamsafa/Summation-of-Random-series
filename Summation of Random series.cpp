73bd21a82067d2c11177ec49774123c90cdfd0de
Mariam Safa <88893709+mariamsafa@users.noreply.github.com>
2021-11-30 23:04:44 +0600

    Add project files.

---
+++ Summation of Random series.cpp
@@ -0,0 +1,37 @@
+//Summation of 1/1^2 + 1/2^2 + 1/3^2 +....+1/N^2 series
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int i, n;
+	double sum = 0;
+	printf("Enter the number : ");
+	scanf_s("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		sum = sum + 1/pow(i, 2);
+	}
+	printf("\nThe summation of this series is %lf \n", sum);
+
+
+	return 0;
+}
+
+//summation of 1/1^1 + 1/2^2 + 1/3^3 +....+1/N^N series
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int i, n;
+	double sum = 0;
+	printf("Enter the number : ");
+	scanf_s("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		sum = sum + 1 / pow(i, i);
+	}
+	printf("\nThe summation of this series is %lf \n", sum);
+
+
+	return 0;
+}
